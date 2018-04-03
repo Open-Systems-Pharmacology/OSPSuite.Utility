e3bf4318ee2267d10f02021090b373d294132fc0
Michael Sevestre <michael@design2code.ca>
2018-04-03 10:59:01 -0400

    Fixes #13 update csv export

--- src/OSPSuite.Utility/Extensions/DataTableExtensions.cs
+++ src/OSPSuite.Utility/Extensions/DataTableExtensions.cs
@@ -9,28 +9,81 @@ namespace OSPSuite.Utility.Extensions
 {
    public static class DataTableExtensions
    {
+      public static DataColumn AddColumn(this DataTable dataTable, string columnName)
+      {
+         return AddColumn<string>(dataTable, columnName);
+      }
+
+      public static DataColumn AddColumn(this DataTable dataTable, string columnName, Type type)
+      {
+         return AddColumn(dataTable, new DataColumn(columnName, type));
+      }
+
+      public static DataColumn AddColumn(this DataTable dataTable, DataColumn dataColumn)
+      {
+         dataTable.Columns.Add(dataColumn);
+         return dataColumn;
+      }
+
+      public static DataColumn AddColumn<T>(this DataTable dataTable, string columnName)
+      {
+         return AddColumn(dataTable, columnName, typeof(T));
+      }
+
+      public static void AddColumns<T>(this DataTable dataTable, params string[] columnNames)
+      {
+         AddColumns<T>(dataTable, columnNames as IEnumerable<string>);
+      }
+
+      public static void AddColumns<T>(this DataTable dataTable, IEnumerable<string> columnNames)
+      {
+         columnNames.Each(name => dataTable.AddColumn<T>(name));
+      }
+
+      public static IReadOnlyList<T> AllValuesInColumn<T>(this DataTable dataTable, string columnName)
+      {
+         return dataTable.Rows.Cast<DataRow>().Select(row => row[columnName].ConvertedTo<T>()).ToList();
+      }
+
+      public static IReadOnlyList<T> AllValuesInColumn<T>(this DataView dataView, string columnName)
+      {
+         return dataView.Cast<DataRowView>().Select(row => row[columnName].ConvertedTo<T>()).ToList();
+      }
+
+      public static DataTable SubTable(this DataTable dataTable, IReadOnlyCollection<string> columnNames, bool distinctValues = true)
+      {
+         if (columnNames.Count == 0)
+            return new DataTable();
+
+         return dataTable.DefaultView.ToTable(distinctValues, columnNames.ToArray());
+      }
+
       /// <summary>
       ///    Exports the datable to csv using the <paramref name="delimiter" /> given as parameter
       /// </summary>
       /// <param name="dataTable">DataTable to export</param>
       /// <param name="fileName">File where the <paramref name="dataTable" /> will be saved</param>
-      /// <param name="delimiter">Delimiter</param>
+      /// <param name="delimiter">Delimiter. Default is ','</param>
+      /// <param name="encloseHeaderInQuotes">
+      ///    If set to <c>true</c> table column will be enclosed in quotes. Default is
+      ///    <c>true</c>
+      /// </param>
       /// <param name="comments">
       ///    List of comment that will be added at the beginning of the csv file. A # will be added at the
       ///    beginning of each item
       /// </param>
-      public static void ExportToCSV(this DataTable dataTable, string fileName, string delimiter = ";", IReadOnlyList<string> comments = null)
+      public static void ExportToCSV(this DataTable dataTable, string fileName, string delimiter = ",", bool encloseHeaderInQuotes = true, IReadOnlyList<string> comments = null)
       {
-         FileHelper.TrySaveFile(fileName, () => { exportToCSV(dataTable, fileName, delimiter, comments); });
+         FileHelper.TrySaveFile(fileName, () => { exportToCSV(dataTable, fileName, delimiter, encloseHeaderInQuotes, comments); });
       }
 
-      private static void exportToCSV(DataTable dataTable, string fileName, string delimiter, IReadOnlyList<string> comments)
+      private static void exportToCSV(DataTable dataTable, string fileName, string delimiter, bool encloseHeaderInQuotes, IReadOnlyList<string> comments)
       {
          using (var sw = new StreamWriter(new FileStream(fileName, FileMode.Create, FileAccess.ReadWrite), Encoding.UTF8))
          {
             var allComments = comments ?? new List<string>();
 
-            //Write the comments (and split  new lines if defined in one line)
+            //Write the comments (and split  new lines if defined in one line)OSP
             foreach (var comment in allComments.SelectMany(c => c.Split(Environment.NewLine.ToCharArray())))
             {
                sw.WriteLine("#{0}", comment);
@@ -40,7 +93,11 @@ namespace OSPSuite.Utility.Extensions
             int colCount = dataTable.Columns.Count;
             for (int i = 0; i < colCount; i++)
             {
-               sw.Write(dataTable.Columns[i]);
+               var columnName = dataTable.Columns[i].ColumnName;
+               if (encloseHeaderInQuotes)
+                  columnName = $"\"{columnName}\"";
+
+               sw.Write(columnName);
                if (i < colCount - 1)
                   sw.Write(delimiter);
             }
@@ -60,6 +117,7 @@ namespace OSPSuite.Utility.Extensions
                   if (i < colCount - 1)
                      sw.Write(delimiter);
                }
+
                sw.Write(sw.NewLine);
             }
 
---
+++ tests/OSPSuite.Utility.Tests/DataTableExtensionsSpecs.cs
@@ -0,0 +1,135 @@
+﻿using System.Data;
+using System.IO;
+using OSPSuite.BDDHelper;
+using OSPSuite.BDDHelper.Extensions;
+using OSPSuite.Utility.Extensions;
+
+namespace OSPSuite.Utility.Tests
+{
+   public abstract class concern_for_DataTableExtensions : StaticContextSpecification
+   {
+      protected DataTable _dataTable;
+
+      public override void GlobalContext()
+      {
+         base.GlobalContext();
+         _dataTable = new DataTable("TEST");
+
+         _dataTable.AddColumn<double>("col1");
+         _dataTable.AddColumn<double>("col2");
+
+         var row = _dataTable.NewRow();
+         row["col1"] = 1.5;
+         row["col2"] = 2.8;
+         _dataTable.Rows.Add(row);
+      }
+   }
+
+   public class When_exporting_a_datatable_to_csv_using_the_default_settings : concern_for_DataTableExtensions
+   {
+      private string _fileName;
+      private string[] _allLines;
+
+      public override void GlobalContext()
+      {
+         base.GlobalContext();
+         _fileName = FileHelper.GenerateTemporaryFileName();
+         _dataTable.ExportToCSV(_fileName);
+         _allLines = File.ReadAllLines(_fileName);
+      }
+
+      [Observation]
+      public void should_have_exported_one_row_for_the_header_and_one_for_each_row()
+      {
+         _allLines.Length.ShouldBeEqualTo(_dataTable.Rows.Count + 1);
+      }
+
+      [Observation]
+      public void should_export_the_datatable_to_a_file_using_the_comma_separator()
+      {
+         var header = _allLines[0].Split(',');
+         header.Length.ShouldBeEqualTo(2);
+      }
+
+      [Observation]
+      public void should_have_added_quotes_between_parameter_names()
+      {
+         var header = _allLines[0].Split(',');
+         header[0].ShouldBeEqualTo("\"col1\"");
+         header[1].ShouldBeEqualTo("\"col2\"");
+      }
+
+      public override void GlobalCleanup()
+      {
+         base.GlobalCleanup();
+         FileHelper.DeleteFile(_fileName);
+      }
+   }
+
+   public class When_exporting_a_datatable_to_csv_using_another_separator_and_without_quote_encapsulation_for_header : concern_for_DataTableExtensions
+   {
+      private string _fileName;
+      private string[] _allLines;
+
+      public override void GlobalContext()
+      {
+         base.GlobalContext();
+         _fileName = FileHelper.GenerateTemporaryFileName();
+         _dataTable.ExportToCSV(_fileName, delimiter: "-", encloseHeaderInQuotes: false);
+         _allLines = File.ReadAllLines(_fileName);
+      }
+
+      [Observation]
+      public void should_have_exported_one_row_for_the_header_and_one_for_each_row()
+      {
+         _allLines.Length.ShouldBeEqualTo(_dataTable.Rows.Count + 1);
+      }
+
+      [Observation]
+      public void should_export_the_datatable_to_a_file_using_the_comma_separator()
+      {
+         var header = _allLines[0].Split('-');
+         header.Length.ShouldBeEqualTo(2);
+      }
+
+      [Observation]
+      public void should_have_added_quotes_between_parameter_names()
+      {
+         var header = _allLines[0].Split('-');
+         header[0].ShouldBeEqualTo("col1");
+         header[1].ShouldBeEqualTo("col2");
+      }
+
+      public override void GlobalCleanup()
+      {
+         base.GlobalCleanup();
+         FileHelper.DeleteFile(_fileName);
+      }
+   }
+
+   public class When_exporting_a_datatable_to_csv_with_comments : concern_for_DataTableExtensions
+   {
+      private string _fileName;
+      private string[] _allLines;
+
+      public override void GlobalContext()
+      {
+         base.GlobalContext();
+         _fileName = FileHelper.GenerateTemporaryFileName();
+         _dataTable.ExportToCSV(_fileName, comments: new[] {"Hello"});
+         _allLines = File.ReadAllLines(_fileName);
+      }
+
+      [Observation]
+      public void should_have_exported_one_row_for_the_header_and_one_for_each_row_and_one_for_each_comment()
+      {
+         _allLines.Length.ShouldBeEqualTo(_dataTable.Rows.Count + 1 + 1);
+      }
+
+      public override void GlobalCleanup()
+      {
+         base.GlobalCleanup();
+         FileHelper.DeleteFile(_fileName);
+      }
+   }
+}
