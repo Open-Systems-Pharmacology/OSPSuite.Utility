814b6c2723459220a48ee1a656360a8238d07878
Michael Sevestre <michael@design2code.ca>
2019-02-13 18:22:30 -0500

    Remove unused files

--- src/OSPSuite.Utility/Extensions/ColorExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Drawing;
-
-namespace OSPSuite.Utility.Extensions
-{
-   public static class ColorExtensions
-   {
-      public static string ToHexString(this Color c) => $"#{c.R:X2}{c.G:X2}{c.B:X2}";
-   }
-}
