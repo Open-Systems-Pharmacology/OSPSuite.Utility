1ac7a28f380029407c94439f95cc2dd0b7b05c40
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-08-26 14:05:21 -0400

    Fixes #33 BinaryFormatter is not supported and by default throws an exception

--- src/OSPSuite.Utility/Conversion/ByteArrayConverter.cs
+++ src/OSPSuite.Utility/Conversion/ByteArrayConverter.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
 using OSPSuite.Utility.Extensions;
 
 namespace OSPSuite.Utility.Conversion
@@ -11,20 +12,30 @@ namespace OSPSuite.Utility.Conversion
          if (typeof(T).IsAnImplementationOf<byte>())
             return arrayToConvert as byte[];
 
-         var formatter = new BinaryFormatter();
-         using (var mem = new MemoryStream())
-         {
-            formatter.Serialize(mem, arrayToConvert);
-            return mem.ToArray();
-         }
+         return JsonSerializer.SerializeToUtf8Bytes(arrayToConvert);
       }
 
       public T[] ConvertFromByteArray<T>(byte[] byteArray)
       {
+         try
+         {
+            return JsonSerializer.Deserialize<T[]>(byteArray);
+         }
+         catch (JsonException)
+         {
+            return convertFromByteArrayUnsafe<T>(byteArray);
+         }
+      }
+
+      /// <summary>
+      ///    Provides compatibility for saved projects. Must be removed after .NET8
+      /// </summary>
+      private T[] convertFromByteArrayUnsafe<T>(byte[] byteArray)
+      {
          var formatter = new BinaryFormatter();
          using (var mem = new MemoryStream(byteArray))
          {
-            return (T[]) formatter.Deserialize(mem);
+            return (T[])formatter.Deserialize(mem);
          }
       }
    }
